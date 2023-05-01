0370deac31e8945fe1217efadae6be625c3e6164
Joe Auz <joseph.auz77@gmail.com>
2023-05-01 13:29:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SDEV265_Final_Project
+SDEV265 Final Project Repository
+
+# Contributors
+### Joe Auz
+### Caleb Luna
+### Joe Prater
---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<Header />
+		<router-view />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+//import MainView from './components/MainView.vue'
+import Footer from './components/Footer.vue';
+
+export default {
+	name: 'App',
+	components: {
+		Header,
+		Footer,
+	},
+
+};
+</script>
+
+<style></style>
